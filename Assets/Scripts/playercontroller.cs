27bd8be0b8d7b9f3b9f5788fce397cc6c1cb4f7b
G_Williams <gdwilliams1234@gmail.com>
2018-03-21 20:43:08 -0500

    cube and jump
    added a cube and a jump to the game both not helpful just entertaining.

--- Assets/Scripts/playercontroller.cs
+++ Assets/Scripts/playercontroller.cs
@@ -5,19 +5,26 @@ using UnityEngine.UI;
 
 
 public class playercontroller : MonoBehaviour {
+	
     private Rigidbody rb;
     private int count;
-    public float speed;
-    public Text countText;
+	public float speed;
+	public Text countText;
     public Text winText;
-    private void Start()
-    {
+
+	private bool onground = false;
+
+	private void Start ()
+
+	{
         rb = GetComponent<Rigidbody>();
         speed = 10;
         count = 0;
         SetCountText();
         winText.text = "";
     }
+
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Pick Up"))
@@ -28,6 +35,7 @@ public class playercontroller : MonoBehaviour {
         }
     }
 
+
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
@@ -36,16 +44,28 @@ public class playercontroller : MonoBehaviour {
             winText.text = "You Win!";
         }
     }
+
+
     void FixedUpdate ()
         {
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
+			Vector3 jump = new Vector3 (0, 250, 0); 
 
-            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
+            Vector3 movement = new Vector3(moveHorizontal, 0.15f, moveVertical);
 
             rb.AddForce(movement * speed);
+
+		if (Input.GetKeyDown(KeyCode.Space) && onground){
+			rb.AddForce (jump);
+			onground = false;
+			}
         }
 
-    
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.CompareTag("ground")) {
+			onground = true;
+		}
+	
+	}
 }
